80dba5ae2eb7991a3628354898a298332894771f
Bruno Blais <blais.bruno@gmail.com>
2025-12-08 14:11:30 +0100

    add contrib utilities

---
+++ contrib/utilities/automatic_launch/launch_lethe_cluster.py
@@ -0,0 +1,20 @@
+"""
+Summary: Script to launch all the Lethe simulations on a cluster, one job for every cases.
+"""
+
+import os
+
+PATH = os.getcwd()
+
+# User input
+CASE_PREFIX = 'cylinder_u_'
+SHELL_FILE = 'launch_lethe.sh'
+
+for root, directories, files in os.walk(PATH):
+
+    if CASE_PREFIX in root:
+
+        os.chdir(root)
+
+        case_name = root.split('/')[-1]
+        os.system(f'sbatch -J {case_name} {SHELL_FILE}')
